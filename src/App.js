9e4d8e4727af89d9618e771f82370b32d27867e1
haminse <haminse@naver.com>
2023-02-13 17:21:27 -0800

    upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const Button = (props) => {
+  return (
+    <div className="button">
+      <a href = {props.link} target="_blank" rel="noreferrer">
+      <img src={props.imgSrc} alt={props.altText} />
+      </a>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>TOY WEB PROJECT</h1>
+      <h4>by MINSE</h4>
+      <Button
+        imgSrc="img/001.png"
+        altText="Button 1"
+        link = 'https://youtubehot.netlify.app'
+      />
+      <Button
+        imgSrc="img/002.png"
+        altText="Button 2"
+        link = 'https://equal-split.netlify.app'
+
+      />
+      <Button
+        imgSrc="img/003.png"
+        altText="Button 3"
+        link = 'https://book-mbti-minse.netlify.app'
+
+      />
+      <Button
+        imgSrc="img/004.png"
+        altText="Button 4"
+        link = 'https://sjsu-facilities-open-notofficial.netlify.app'
+
+      />
+      <Button
+        imgSrc="img/005.png"
+        altText="Button 5"
+        link = 'https://zodiaccheck.netlify.app/'
+
+      />
+      <Button
+        imgSrc="img/006.png"
+        altText="Button 6"
+        link = 'https://balance-game.netlify.app'
+
+      />
     </div>
   );
 }
